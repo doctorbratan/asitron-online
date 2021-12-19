d1c46ebef6c9df50820a92e5c05554db50461b5c
Doctor Bratan <doctorbratan@gmail.com>
2021-12-19 05:41:52 +0200

    vue-on

---
+++ main.js
@@ -0,0 +1,50 @@
+new Vue({
+    el: '#vue',
+    data: {
+        categories: [],
+        items: [],
+        api: false,
+        text: undefined,
+        category: undefined
+    },
+    mounted(){
+        this.categoriesLoad()
+        // this.onloadSearch()
+    },
+    methods:{
+
+        categoriesLoad() {
+            axios
+                .get('https://asitron.herokuapp.com/api/website/category')
+                .then( (response) => {
+                    this.categories = response.data 
+                    this.category = this.categories[0]
+                } )
+        },
+
+        onloadSearch()  {
+            axios
+            .post('https://asitron.herokuapp.com/api/website/items', {
+                text: "iphone",
+                category: "Чехлы"
+            })
+            .then( (response) => {
+                this.items = response.data
+            }) 
+        },
+
+        search() {
+             axios
+                .post('https://asitron.herokuapp.com/api/website/items', {
+                    text: this.text,
+                    category: this.category
+                })
+                .then( (response) => {
+                    this.api = true
+                    this.items = response.data
+                    console.log(response.data)
+                }) 
+        }
+
+    },
+})
